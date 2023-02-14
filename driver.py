b1f34aeab6ad20811b8fc2546b2731cb398a2345
pra33g <prakhargupta3301@gmail.com>
2023-02-14 22:53:41 +0530

    ce,pe working

---
+++ driver.py
@@ -0,0 +1,68 @@
+import pandas as pd
+stock = "nifty"
+df = pd.read_csv(f"{stock}.csv")
+
+days = df['Day']
+weekDays = [
+    "Friday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+]
+idx_weeks = 0
+weeks = [[]]
+for idx_days, day in enumerate(days):
+    if pd.isna(day):
+        continue
+    # weeks[idx_weeks].append(df.loc[idx_days].values.tolist())
+    weeks[idx_weeks].append(df.loc[idx_days])
+    if not pd.isna(days[idx_days + 1]) and weekDays.index(days[idx_days]) >= weekDays.index(days[idx_days + 1]):
+        idx_weeks += 1
+        weeks.append([])
+
+week_df = pd.DataFrame(columns = [
+    "Date",
+    "Day",
+    "WeekOpen",
+    "WeekHigh",
+    "WeekLow",
+    "WeekClose",
+    "High-Low",
+])
+
+for i, week in enumerate(weeks):
+    w_high = week[0]["High"]
+    w_low = week[0]["Low"]
+    w_open = week[0]["Open"]
+    w_close = week[0]["Close"]
+    w_date = week[-1]["Date"]
+    w_day = week[-1]["Day"]
+    for day in week:
+        if day["High"] > w_high:
+            w_high = day["High"]
+        if day["Low"] < w_low:
+            w_low = day["Low"]    
+    # print(w_date, w_day, w_high, w_low, w_close, i)
+    week_df.loc[len(week_df)] = [
+        w_date,
+        w_day,
+        w_open,
+        w_high,
+        w_low,
+        w_close,
+        w_high - w_low
+    ]
+
+year_hl_mean = week_df['High-Low'].mean()
+year_hl_median = week_df['High-Low'].median()
+
+ce = []
+pe = []
+for i,val in enumerate(week_df['WeekClose']):
+    ce.append(val + year_hl_mean / 2)
+    pe.append(val - year_hl_mean / 2)
+
+week_df['CE'] = ce
+week_df['PE'] = pe
+print(week_df)
