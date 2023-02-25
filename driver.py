67e9498a1fe26fbd6a7848c25d046f30644e4502
pra33g <prakhargupta3301@gmail.com>
2023-02-25 21:00:56 +0530

    import date time modules

--- driver.py
+++ driver.py
@@ -1,14 +1,24 @@
 from ironCondor import IronCondor
-import datetime
+from datetime import datetime, date, time, timedelta
+
 
 
 ##
 stock = "NIFTY"
 entry_day = None
-entry_time = datetime.time(15,14,59)
+entry_time = time(15, 14, 59)
 exit_day = None
-exit_time = datetime.time(15,14,59)
+exit_time = time(15,14,59)
 
 
 ic = IronCondor("NIFTY", "ic-backtest.csv", entry_day, entry_time , exit_day, exit_time)
 ic.ironCondorAlgorithm()
+
+####testing
+# tt = time(0,0,1)
+# for i in range(0, 5):
+#     entry_time = datetime.combine(date.min, entry_time) - datetime.combine(date.min, tt)
+#     entry_time = (datetime.min + entry_time).time()
+#     print(type(entry_time))
+
+
